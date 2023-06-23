cc08277517cc5ce57ee1a31b248255d0ea4b5c86
DukeXCode <naim.giger@outlook.com>
2023-06-23 11:23:12 +0200

    remove unnecessary whitespace

--- src/main/kotlin/ch/dukex/chat/server/repository/PersonRepository.kt
+++ src/main/kotlin/ch/dukex/chat/server/repository/PersonRepository.kt
@@ -3,5 +3,4 @@ package ch.dukex.chat.server.repository
 import ch.dukex.chat.server.model.Person
 import org.springframework.data.jpa.repository.JpaRepository
 
-
 interface PersonRepository : JpaRepository<Person, Long>
